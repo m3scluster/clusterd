6b0efde29c892168fac59970724998aa0b68c07d
Devin Leamy <dleamy@twitter.com>
2024-03-26 16:51:33 -0400

    [cgroups2] Introduces interface to obtain CPU usage statistics.
    The "cpu.stat" control file contains statistics about CPU usage. We capture a
    snapshot of "cpu.stat" inside of a `cpu::Stats` object and expose it through
    `cgroups2::cpu::stats`.
    
    This closes #531

--- src/linux/cgroups2.cpp
+++ src/linux/cgroups2.cpp
@@ -58,6 +58,44 @@ Try<Nothing> write(
     const T& value);
 
 
+template <>
+Try<string> read(const string& cgroup, const string& control)
+{
+  return os::read(path::join(cgroups2::path(cgroup), control));
+}
+
+
+template <>
+Try<uint64_t> read(const string& cgroup, const string& control)
+{
+  Try<string> content = read<string>(cgroup, control);
+  if (content.isError()) {
+    return Error(content.error());
+  }
+
+  return numify<uint64_t>(strings::trim(*content));
+}
+
+
+template <>
+Try<Nothing> write(
+    const string& cgroup,
+    const string& control,
+    const string& value)
+{
+  return os::write(path::join(cgroups2::path(cgroup), control), value);
+}
+
+
+template <>
+Try<Nothing> write(
+    const string& cgroup,
+    const string& control,
+    const uint64_t& value)
+{
+  return write(cgroup, control, stringify(value));
+}
+
 namespace control {
 
 // Interface files found in all cgroups.
@@ -106,6 +144,13 @@ struct State
     _disabled.insert(controller);
   }
 
+  void disable(const set<string>& controllers)
+  {
+    foreach (const string& controller, controllers) {
+      disable(controller);
+    }
+  }
+
   set<string> enabled()  const { return _enabled; }
   set<string> disabled() const { return _disabled; }
 
@@ -148,48 +193,31 @@ std::ostream& operator<<(std::ostream& stream, const State& state)
   return stream;
 }
 
+
+Try<State> read(const string& cgroup)
+{
+  Try<string> contents =
+    cgroups2::read<string>(cgroup, cgroups2::control::SUBTREE_CONTROLLERS);
+
+  if (contents.isError()) {
+    return Error(
+        "Failed to read 'cgroup.subtree_control' for cgroup '" + cgroup + "': "
+        + contents.error());
+  }
+
+  return State::parse(*contents);
+}
+
+
+Try<Nothing> write(const string& cgroup, const State& state)
+{
+  return cgroups2::write(
+      cgroup, control::SUBTREE_CONTROLLERS, stringify(state));
+}
+
 } // namespace subtree_control {
 
 } // namespace control {
-
-
-template <>
-Try<string> read(const string& cgroup, const string& control)
-{
-  return os::read(path::join(cgroups2::path(cgroup), control));
-}
-
-
-template <>
-Try<uint64_t> read(const string& cgroup, const string& control)
-{
-  Try<string> content = read<string>(cgroup, control);
-  if (content.isError()) {
-    return Error(content.error());
-  }
-
-  return numify<uint64_t>(strings::trim(*content));
-}
-
-
-template <>
-Try<Nothing> write(
-    const string& cgroup,
-    const string& control,
-    const string& value)
-{
-  return os::write(path::join(cgroups2::path(cgroup), control), value);
-}
-
-
-template <>
-Try<Nothing> write(
-    const string& cgroup,
-    const string& control,
-    const uint64_t& value)
-{
-  return write(cgroup, control, stringify(value));
-}
 
 
 bool enabled()
@@ -418,20 +446,29 @@ Try<set<string>> available(const string& cgroup)
 
 Try<Nothing> enable(const string& cgroup, const vector<string>& controllers)
 {
-  Try<string> contents =
-    cgroups2::read<string>(cgroup, cgroups2::control::SUBTREE_CONTROLLERS);
+  using State = control::subtree_control::State;
+  Try<State> control = cgroups2::control::subtree_control::read(cgroup);
 
-  if (contents.isError()) {
-    return Error(contents.error());
+  if (control.isError()) {
+    return Error(control.error());
   }
 
+  control->enable(controllers);
+  return cgroups2::control::subtree_control::write(cgroup, *control);
+}
+
+
+Try<Nothing> disable(const string& cgroup, const set<string>& controllers)
+{
   using State = control::subtree_control::State;
-  State control = State::parse(*contents);
-  control.enable(controllers);
-  return cgroups2::write(
-      cgroup,
-      control::SUBTREE_CONTROLLERS,
-      stringify(control));
+  Try<State> control = cgroups2::control::subtree_control::read(cgroup);
+
+  if (control.isError()) {
+    return Error(control.error());
+  }
+
+  control->disable(controllers);
+  return cgroups2::control::subtree_control::write(cgroup, *control);
 }
 
 
@@ -465,6 +502,48 @@ const std::string UCLAMP_MIN = "cpu.uclamp.min";
 const std::string WEIGHT = "cpu.weight";
 const std::string WEIGHT_NICE = "cpu.weight.nice";
 
+namespace stat {
+
+Try<Stats> parse(const string& content)
+{
+  const vector<string> lines = strings::split(content, "\n");
+  cpu::Stats stats;
+
+  foreach (const string& line, lines) {
+    if (line.empty()) {
+      continue;
+    }
+
+    vector<string> tokens = strings::split(line, " ");
+    if (tokens.size() != 2) {
+      return Error("Invalid line format in 'cpu.stat' expected "
+                   "<key> <value> received: '" + line + "'");
+    }
+
+    const string& field = tokens[0];
+    const string& value = tokens[1];
+
+    Try<uint64_t> number = numify<uint64_t>(value);
+    if (number.isError()) {
+      return Error("Failed to parse '" + field + "': " + number.error());
+    }
+    Duration duration = Microseconds(static_cast<int64_t>(*number));
+
+    if      (field == "usage_usec")     { stats.usage = duration; }
+    else if (field == "user_usec")      { stats.user_time = duration; }
+    else if (field == "system_usec")    { stats.system_time = duration; }
+    else if (field == "nr_periods")     { stats.periods = *number; }
+    else if (field == "nr_throttled")   { stats.throttled = *number; }
+    else if (field == "throttled_usec") { stats.throttle_time = duration; }
+    else if (field == "nr_burst")       { stats.bursts = *number; }
+    else if (field == "burst_usec")     { stats.bursts_time = duration; }
+  }
+
+  return stats;
+}
+
+} // namespace stat {
+
 } // namespace control {
 
 Try<Nothing> weight(const string& cgroup, uint64_t weight)
@@ -484,6 +563,20 @@ Try<uint64_t> weight(const string& cgroup)
   }
 
   return cgroups2::read<uint64_t>(cgroup, cpu::control::WEIGHT);
+}
+
+
+Try<cpu::Stats> stats(const string& cgroup)
+{
+  Try<string> content = cgroups2::read<string>(
+      cgroup, cgroups2::cpu::control::STATS);
+
+  if (content.isError()) {
+    return Error("Failed to read 'cpu.stat' for the cgroup '" + cgroup + "': "
+                 + content.error());
+  }
+
+  return cpu::control::stat::parse(*content);
 }
 
 } // namespace cpu {
--- src/tests/containerizer/cgroups2_tests.cpp
+++ src/tests/containerizer/cgroups2_tests.cpp
@@ -16,18 +16,32 @@
 
 #include <set>
 #include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
 
 #include <process/reap.hpp>
+#include <process/gmock.hpp>
 #include <process/gtest.hpp>
 
 #include <stout/exit.hpp>
+#include <stout/foreach.hpp>
+#include <stout/gtest.hpp>
+#include <stout/os.hpp>
+#include <stout/set.hpp>
 #include <stout/tests/utils.hpp>
 #include <stout/try.hpp>
 
 #include "linux/cgroups2.hpp"
+#include "linux/ebpf.hpp"
 
+using std::pair;
 using std::set;
 using std::string;
+using std::tuple;
+using std::vector;
+
+namespace devices = cgroups2::devices;
 
 namespace mesos {
 namespace internal {
@@ -40,6 +54,29 @@ const string TEST_CGROUP = "test";
 class Cgroups2Test : public TemporaryDirectoryTest
 {
 protected:
+  Try<Nothing> enable_controllers(const vector<string>& controllers)
+  {
+    Try<set<string>> enabled = cgroups2::controllers::enabled(
+        cgroups2::ROOT_CGROUP);
+
+    if (enabled.isError()) {
+      return Error("Failed to check enabled controllers: " + enabled.error());
+    }
+
+    set<string> to_enable(controllers.begin(), controllers.end());
+    to_enable = to_enable - *enabled;
+
+    Try<Nothing> result = cgroups2::controllers::enable(
+        cgroups2::ROOT_CGROUP,
+        vector<string>(to_enable.begin(), to_enable.end()));
+
+    if (result.isSome()) {
+      enabled_controllers = enabled_controllers | to_enable;
+    }
+
+    return result;
+  }
+
   void SetUp() override
   {
     TemporaryDirectoryTest::SetUp();
@@ -57,8 +94,16 @@ protected:
       ASSERT_SOME(cgroups2::destroy(TEST_CGROUP));
     }
 
+    ASSERT_SOME(cgroups2::controllers::disable(
+        cgroups2::ROOT_CGROUP, enabled_controllers));
+
     TemporaryDirectoryTest::TearDown();
   }
+
+  // These are controllers that *we* enabled, and therefore we need to
+  // disable on test cleanup. Note that if the tests are killed, then
+  // we'll still of course leak these side effects, unfortunately.
+  set<string> enabled_controllers;
 };
 
 
@@ -133,6 +178,190 @@ TEST_F(Cgroups2Test, ROOT_CGROUPS2_AssignProcesses)
   ASSERT_NE(-1, ::kill(pid, SIGKILL));
   AWAIT_EXPECT_WTERMSIG_EQ(SIGKILL, process::reap(pid));
 }
+
+
+TEST_F(Cgroups2Test, ROOT_CGROUPS2_CpuStats)
+{
+  ASSERT_SOME(enable_controllers({"cpu"}));
+
+  ASSERT_SOME(cgroups2::create(TEST_CGROUP));
+  ASSERT_SOME(cgroups2::controllers::enable(TEST_CGROUP, {"cpu"}));
+  ASSERT_SOME(cgroups2::cpu::stats(TEST_CGROUP));
+}
+
+
+TEST_F(Cgroups2Test, ROOT_CGROUPS2_EnableAndDisable)
+{
+  ASSERT_SOME(enable_controllers({"cpu"}));
+
+  ASSERT_SOME(cgroups2::create(TEST_CGROUP));
+
+  // Check that "cpu" not enabled.
+  Try<set<string>> enabled = cgroups2::controllers::enabled(TEST_CGROUP);
+  EXPECT_SOME(enabled);
+  EXPECT_EQ(0u, enabled->count("cpu"));
+
+  // Enable "cpu".
+  EXPECT_SOME(cgroups2::controllers::enable(TEST_CGROUP, {"cpu"}));
+  EXPECT_SOME(cgroups2::controllers::enable(TEST_CGROUP, {"cpu"})); // NOP
+
+  // Check that "cpu" is enabled.
+  enabled = cgroups2::controllers::enabled(TEST_CGROUP);
+  EXPECT_SOME(enabled);
+  EXPECT_EQ(1u, enabled->count("cpu"));
+
+  // Disable "cpu".
+  EXPECT_SOME(cgroups2::controllers::disable(TEST_CGROUP, {"cpu"}));
+  EXPECT_SOME(cgroups2::controllers::disable(TEST_CGROUP, {"cpu"})); // NOP
+
+  // Check that "cpu" not enabled.
+  enabled = cgroups2::controllers::enabled(TEST_CGROUP);
+  EXPECT_SOME(enabled);
+  EXPECT_EQ(0u, enabled->count("cpu"));
+}
+
+// Arguments for os::open(). Combination of a path and an access type.
+typedef pair<string, int> OpenArgs;
+
+using DeviceControllerTestParams = tuple<
+    vector<devices::Entry>,
+    vector<devices::Entry>,
+    vector<OpenArgs>,
+    vector<OpenArgs>>;
+
+class DeviceControllerTestFixture :
+    public Cgroups2Test,
+    public ::testing::WithParamInterface<DeviceControllerTestParams> {};
+
+
+TEST_P(DeviceControllerTestFixture, ROOT_CGROUPS2_DeviceController)
+{
+  const string& cgroup = TEST_CGROUP;
+
+  auto params = GetParam();
+  const vector<devices::Entry>& allow = std::get<0>(params);
+  const vector<devices::Entry>& deny = std::get<1>(params);
+  const vector<OpenArgs>& allowed_accesses = std::get<2>(params);
+  const vector<OpenArgs>& blocked_accesses = std::get<3>(params);
+
+  ASSERT_SOME(cgroups2::create(cgroup));
+  string path = cgroups2::path(cgroup);
+
+  Try<vector<uint32_t>> attached = ebpf::cgroups2::attached(path);
+  ASSERT_SOME(attached);
+  ASSERT_EQ(0u, attached->size());
+
+  ASSERT_SOME(devices::configure(cgroup, allow, deny));
+  attached = ebpf::cgroups2::attached(path);
+  ASSERT_SOME(attached);
+  ASSERT_EQ(1u, attached->size());
+
+  pid_t pid = ::fork();
+  ASSERT_NE(-1, pid);
+
+  if (pid == 0) {
+    // Move the child process into the newly created cgroup.
+    Try<Nothing> assign = cgroups2::assign(cgroup, ::getpid());
+    if (assign.isError()) {
+      SAFE_EXIT(EXIT_FAILURE, "Failed to assign child process to cgroup");
+    }
+
+    // Check that we can only do the "allowed_accesses".
+    foreach(const OpenArgs& args, allowed_accesses) {
+      if (os::open(args.first, args.second).isError()) {
+        SAFE_EXIT(EXIT_FAILURE, "Expected allowed read to succeed");
+      }
+    }
+    foreach(const OpenArgs& args, blocked_accesses) {
+      if (os::open(args.first, args.second).isSome()) {
+        SAFE_EXIT(EXIT_FAILURE, "Expected blocked read to fail");
+      }
+    }
+
+    ASSERT_SOME(ebpf::cgroups2::detach(path, attached->at(0)));
+
+    // Check that we can do both the "allowed_accesses" and "blocked_accesses".
+    foreach(const OpenArgs& args, allowed_accesses) {
+      if (os::open(args.first, args.second).isError()) {
+        SAFE_EXIT(EXIT_FAILURE, "Expected successful read after detaching"
+          " device controller program");
+      }
+    }
+    foreach(const OpenArgs& args, blocked_accesses) {
+      if (os::open(args.first, args.second).isError()) {
+        SAFE_EXIT(EXIT_FAILURE, "Expected successful read after detaching"
+          " device controller program");
+      }
+    }
+
+    ::_exit(EXIT_SUCCESS);
+  }
+
+  AWAIT_EXPECT_WEXITSTATUS_EQ(EXIT_SUCCESS, process::reap(pid));
+}
+
+
+INSTANTIATE_TEST_CASE_P(
+    DeviceControllerTestParams,
+    DeviceControllerTestFixture,
+    ::testing::Values<DeviceControllerTestParams>(
+        // Acceses are blocked by default if no entries are configured.
+        DeviceControllerTestParams{
+          vector<devices::Entry>{},
+          vector<devices::Entry>{},
+          vector<OpenArgs>{},
+          vector<OpenArgs>{{os::DEV_NULL, O_RDWR}, {os::DEV_NULL, O_RDWR}}
+        },
+        // Deny access to /dev/null.
+        DeviceControllerTestParams{
+          vector<devices::Entry>{},
+          vector<devices::Entry>{*devices::Entry::parse("c 1:3 rwm")},
+          vector<OpenArgs>{},
+          vector<OpenArgs>{{os::DEV_NULL, O_RDWR}}
+        },
+        // Allow access to /dev/null.
+        DeviceControllerTestParams{
+          vector<devices::Entry>{*devices::Entry::parse("c 1:3 rwm")},
+          vector<devices::Entry>{},
+          vector<OpenArgs>{{os::DEV_NULL, O_RDWR}},
+          vector<OpenArgs>{}
+        },
+        // Allow read-only access to /dev/null.
+        DeviceControllerTestParams{
+          vector<devices::Entry>{*devices::Entry::parse("c 1:3 r")},
+          vector<devices::Entry>{},
+          vector<OpenArgs>{{os::DEV_NULL, O_RDONLY}},
+          vector<OpenArgs>{{os::DEV_NULL, O_RDWR}}
+        },
+        // Allow write-only access to /dev/null.
+        DeviceControllerTestParams{
+          vector<devices::Entry>{*devices::Entry::parse("c 1:3 w")},
+          vector<devices::Entry>{},
+            // write-only allowed
+          vector<OpenArgs>{{os::DEV_NULL, O_WRONLY}},
+            // read is blocked
+          vector<OpenArgs>{{os::DEV_NULL, O_RDWR}, {os::DEV_NULL, O_RDONLY}}
+        },
+        // Access to /dev/null is denied because the allowed access is to
+        // a different device type with the same major:minor numbers.
+        DeviceControllerTestParams{
+          vector<devices::Entry>{*devices::Entry::parse("b 1:3 rwm")},
+          vector<devices::Entry>{},
+          vector<OpenArgs>{},
+            // /dev/null is blocked
+          vector<OpenArgs>{{os::DEV_NULL, O_RDWR}, {os::DEV_NULL, O_RDONLY}}
+        },
+        // Allow access to all devices.
+        DeviceControllerTestParams{
+          vector<devices::Entry>{*devices::Entry::parse("a *:* rwm")},
+          vector<devices::Entry>{},
+          vector<OpenArgs>{
+            {os::DEV_NULL, O_WRONLY},
+            {os::DEV_NULL, O_RDWR},
+            {os::DEV_NULL, O_RDONLY}},
+          vector<OpenArgs>{},
+        }
+      ));
 
 } // namespace tests {
 
